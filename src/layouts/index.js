ff3c58bc12827444a6f50167e865fadb8ec5f380
Maciej001 <m.nowakowskipl@gmail.com>
2017-07-20 19:08:39 +0100

    GA added

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
+import ReactGA from 'react-ga';
+
 import { getTheme } from "../components/styled/Theme";
 import "../components/styled/Global";
 import WebsiteHead from "../components/home/WebsiteHead";
@@ -7,9 +9,27 @@ import Footer from "../components/home/Footer";
 
 // Some css theme params depend on window width.
 class Layout extends Component {
+  constructor( props ) {
+    super( props );
+    this.logPageView = this.logPageView.bind( this );
+    this.changeRoute = this.changeRoute.bind( this );
+  }
+
   state = { windowWidth: 0 };
 
+  changeRoute() {
+    this.logPageView();
+  }
+
+  logPageView() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+
   componentDidMount() {
+
+    ReactGA.initialize( GOOGLE_TRACKING_ID );
 
     const intercom_script = (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xpwec2ck';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
