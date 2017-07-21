7c9eca968af7b6b380b97f7bfca2d690015e872e
Maciej001 <m.nowakowskipl@gmail.com>
2017-07-21 14:08:35 +0100

    Website head

--- src/components/home/WebsiteHead.js
+++ src/components/home/WebsiteHead.js
@@ -3,34 +3,34 @@ import Helmet from 'react-helmet';
 
 const WebsiteHead = () =>
   <Helmet>
-    <title>Indie Dojo - a one week bootcamp for indie hackers</title>
-    <meta name='description' content="A one-week bootcamp for indie hackers, that will give you a detailed, step-by-step guide, how to move from a blank page to the launch-ready web product."/>
-    <link rel="canonical" href="http://www.indiedojo.club" />
+    <title>Indie Dojo - a one-week bootcamp for indie hackers</title>
+    <meta name='description' content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
+    <link rel="canonical" href="http://indiedojo.co" />
     <link rel="shortcut icon" href="/favicon.png" />
 
     <html itemscope itemtype="http://schema.org/Event"/>
 
     {/* Schema.org markup for Google+ */}
-    <meta itemprop="name" content="Indie Dojo - a one week bootcamp for indie hackers"/>
-    <meta itemprop="description" content="A one-week bootcamp for indie hackers, that will give you a detailed, step-by-step guide, how to move from a blank page to the launch-ready web product."/>
-    <meta itemprop="image" content="https://s3-eu-west-1.amazonaws.com/codecamps/courses/IndieDojo/kyoto-main.jpg"/>
+    <meta itemprop="name" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
+    <meta itemprop="description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
+    <meta itemprop="image" content="http://indiedojo/indie-dojo-social.jpg"/>
 
     {/* Twitter Card data */}
     <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:site" content="@IndieDojoClub"/>
-    <meta name="twitter:title" content="Indie Dojo - a one week bootcamp for indie hackers"/>
-    <meta name="twitter:description" content="A one-week bootcamp for indie hackers, that will give you a detailed, step-by-step guide, how to move from a blank page to the launch-ready web product."/>
-    <meta name="twitter:creator" content="@IndieDojoClub"/>
+    <meta name="twitter:site" content="@indiedojoco"/>
+    <meta name="twitter:title" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
+    <meta name="twitter:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
+    <meta name="twitter:creator" content="@indiedojoco"/>
 
     {/* Twitter summary card with large image must be at least 280x150px */}
-    <meta name="twitter:image:src" content="https://s3-eu-west-1.amazonaws.com/codecamps/courses/IndieDojo/kyoto-main.jpg"/>
+    <meta name="twitter:image:src" content="http://indiedojo/indie-dojo-social.jpg"/>
 
     {/* Open Graph data */}
-    <meta property="og:title" content="Indie Dojo - a one week bootcamp for indie hackers" />
+    <meta property="og:title" content="Indie Dojo - a one-week bootcamp for indie hackers" />
     <meta property="og:type" content="event" />
     <meta property="og:url" content="http://www.indiedojo.club/" />
-    <meta property="og:image" content="https://s3-eu-west-1.amazonaws.com/codecamps/courses/IndieDojo/kyoto-main.jpg" />
-    <meta property="og:description" content="A one-week bootcamp for indie hackers, that will give you a detailed, step-by-step guide, how to move from a blank page to the launch-ready web product." />
+    <meta property="og:image" content="http://indiedojo/indie-dojo-social.jpg" />
+    <meta property="og:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product" />
     <meta property="og:site_name" content="Indie Dojo" />
     <meta property="fb:admins" content="1483954624984668" />
 
