420eeb7025a01c70d7823404804e02442092c28f
Maciej001 <m.nowakowskipl@gmail.com>
2017-07-21 11:42:19 +0100

    Moved GA outside of the component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,29 +7,34 @@ import "../components/styled/Global";
 import WebsiteHead from "../components/home/WebsiteHead";
 import Footer from "../components/home/Footer";
 
+ReactGA.initialize( "UA-8858797-9" );
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 // Some css theme params depend on window width.
 class Layout extends Component {
   constructor( props ) {
     super( props );
-    this.logPageView = this.logPageView.bind( this );
-    this.changeRoute = this.changeRoute.bind( this );
+    // this.logPageView = this.logPageView.bind( this );
+    // this.changeRoute = this.changeRoute.bind( this );
   }
 
   state = { windowWidth: 0 };
 
-  changeRoute() {
-    this.logPageView();
-  }
+  // changeRoute() {
+  //   this.logPageView();
+  // }
 
-  logPageView() {
-    ReactGA.set({ page: window.location.pathname + window.location.search });
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }
+  // logPageView() {
+  //   ReactGA.set({ page: window.location.pathname + window.location.search });
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }
 
 
   componentDidMount() {
-
-    ReactGA.initialize( 'UA-8858797-9' );
 
     const intercom_script = (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xpwec2ck';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
