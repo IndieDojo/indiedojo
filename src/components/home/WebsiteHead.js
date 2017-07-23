4b9008c82bd0de3c0b89e8db84a27dbb3315336e
Maciej001 <m.nowakowskipl@gmail.com>
2017-07-23 17:51:09 +0100

    Other metatags

--- src/components/home/WebsiteHead.js
+++ src/components/home/WebsiteHead.js
@@ -11,17 +11,9 @@ const WebsiteHead = () =>
     {/* <html itemscope itemtype="http://schema.org/Event"/> */}
 
     {/* Schema.org markup for Google+ */}
-    {/* <meta itemprop="name" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
+    <meta itemprop="name" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
     <meta itemprop="description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
-    <meta itemprop="image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg"/> */}
-
-    {/* Twitter Card data */}
-    {/* <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:site" content="@indiedojoco"/>
-    <meta name="twitter:creator" content="@indiedojoco"/>
-    <meta name="twitter:title" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
-    <meta name="twitter:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
-    <meta name="twitter:image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg"/> */}
+    <meta itemprop="image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg"/>
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@indiedojoco" />
@@ -31,13 +23,13 @@ const WebsiteHead = () =>
     <meta name="twitter:image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg" />
 
     {/* Open Graph data */}
-    {/* <meta property="og:title" content="Indie Dojo - a one-week bootcamp for indie hackers" />
+    <meta property="og:title" content="Indie Dojo - a one-week bootcamp for indie hackers" />
     <meta property="og:type" content="event" />
     <meta property="og:url" content="http://indiedojo.co/" />
     <meta property="og:image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg" />
     <meta property="og:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product" />
     <meta property="og:site_name" content="Indie Dojo" />
-    <meta property="fb:admins" content="1483954624984668" /> */}
+    <meta property="fb:admins" content="1483954624984668" />
 
   </Helmet>
 
