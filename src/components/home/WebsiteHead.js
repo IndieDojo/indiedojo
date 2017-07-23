f63df3ab3c5df2d16987f5a28b8e64386b133acc
Maciej001 <m.nowakowskipl@gmail.com>
2017-07-23 17:27:17 +0100

    tiny social in Head

--- src/components/home/WebsiteHead.js
+++ src/components/home/WebsiteHead.js
@@ -13,7 +13,7 @@ const WebsiteHead = () =>
     {/* Schema.org markup for Google+ */}
     <meta itemprop="name" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
     <meta itemprop="description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
-    <meta itemprop="image" content="http://indiedojo.co/indie-dojo-social.jpg"/>
+    <meta itemprop="image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg"/>
 
     {/* Twitter Card data */}
     <meta name="twitter:card" content="summary_large_image"/>
@@ -21,13 +21,13 @@ const WebsiteHead = () =>
     <meta name="twitter:creator" content="@indiedojoco"/>
     <meta name="twitter:title" content="Indie Dojo - a one-week bootcamp for indie hackers"/>
     <meta name="twitter:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product"/>
-    <meta name="twitter:image:src" content="http://indiedojo.co/indie-dojo-social.jpg"/>
+    <meta name="twitter:image:src" content="http://indiedojo.co/tiny/indie-dojo-social.jpg"/>
 
     {/* Open Graph data */}
     <meta property="og:title" content="Indie Dojo - a one-week bootcamp for indie hackers" />
     <meta property="og:type" content="event" />
     <meta property="og:url" content="http://indiedojo.co/" />
-    <meta property="og:image" content="http://indiedojo.co/indie-dojo-social.jpg" />
+    <meta property="og:image" content="http://indiedojo.co/tiny/indie-dojo-social.jpg" />
     <meta property="og:description" content="Indie Dojo is a one-week bootcamp that will give you a step-by-step blueprint for moving from a blank page to a launch-ready web product" />
     <meta property="og:site_name" content="Indie Dojo" />
     <meta property="fb:admins" content="1483954624984668" />
